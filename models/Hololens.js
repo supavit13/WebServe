31f86ad785af7a961a45101d51f64c9b391c485c
game <supavit13@gmail.com>
2018-06-29 20:34:54 +0700

    game

--- models/Hololens.js
+++ models/Hololens.js
@@ -5,39 +5,9 @@ var Hololens = new Schema({
     date : {
         type: String
     }
-    data : [
-        {
-            flight: {
-                type: String
-            },
-            first_time: {
-                type: String
-            },
-            lastest_time: {
-                type: String
-            },
-            points: [
-                {
-                    lat : {
-                        type :Number
-                    },
-                    lon : {
-                        type :Number
-                    },
-                    altitude : {
-                        type :Number
-                    },
-                    speed : {
-                        type :Number
-                    },
-                    time : {
-                        type :String
-                    }
-                }
-            ]
-        }
-           
-    ]
+    data : {
+        type : Object
+    }
     
 });
 
