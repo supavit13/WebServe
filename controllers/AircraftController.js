5897cb32e1f4f89bb2ad16ce69aba8903dd3709b
game <supavit13@gmail.com>
2018-07-12 11:05:33 +0700

    game

--- controllers/AircraftController.js
+++ controllers/AircraftController.js
@@ -379,6 +379,7 @@ AircraftController.getdata = function (req, res) {
         if(smonth[0] == '0') smonth = smonth[1];
         if(sday[0] == '0') sday = sday[1];
         if(Older == null) Older = mongoose.model('backup'+sday+smonth+syear,{});
+        console.log(Older);
         Older.find(qry).limit(1000).sort({ unixtime: -1 }).exec(function (err, result) { //limit data 1000 records
             if (err) throw err;
             else {
