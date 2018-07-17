6216a8b466f8bd36b66320d2fb50fa220eb4d0cc
game <supavit13@gmail.com>
2018-07-17 10:07:39 +0700

    game

--- app.js
+++ app.js
@@ -40,15 +40,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({limit: '50mb' , extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-app.use(bodyParser.json({ limit: '30mb' }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb' , extended: true}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
